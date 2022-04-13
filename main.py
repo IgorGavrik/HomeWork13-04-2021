1873b3260573b60c1bbc6b3e576778ac2bcc972d
mac <igor.gavrik@tut.by>
2022-04-13 15:45:31 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# pip install django
+# You should consider upgrading via the
+# '/Users/mac/PycharmProjects/pythonProject2/HomeWork13-04-2021/venv/bin/python -m pip install --upgrade pip' command.
+
+# django-admin startproject config
+# пометить папку проекта как "source root"
+# переименовываем папку app в проекте на config
+# cd app
+# python manage.py startapp form
+# переходим в файл settings.py и добавляем в приложения
+# пишем код во views
+# python manage.py runserver
